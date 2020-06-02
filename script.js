c7c5e29032decc7e3a0061c08a4456612da51d82
Trevor-Warthman <43298111+Trevor-Warthman@users.noreply.github.com>
2020-06-02 10:09:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,42 @@ function colorCheckBox(id){
 
 }
 
-//To be added to later
+//Display random encouragement
 function encourage(){
-	alert("Good job! You completed your task!")
+	const rand = Math.floor(Math.random() * 10
+	switch(expression) {
+  		case 0:
+			alert("Yay you did it")
+    		break
+  		case 1:
+			alert("great work!")
+		break
+		case 2:
+			alert("I'm proud of you.")
+		break
+		case 3:
+			alert("Nice!")
+		break
+		case 4:
+			alert("Good job.")
+		break
+		case 5:
+			alert("One step closer to being done!")
+		break
+		case 6:
+			alert("You are unstoppable")
+		break
+		case 7:
+			alert("See? That wasn't so hard!")
+		break
+		case 8:
+			alert("Anything can be accomplished with the help of a great todo list!")
+		break
+		case 9:
+			alert("Good job! You completed your task!")
+		break
+		default:
+			alert("Good job! You completed your task!")
+			
+	}
 }
